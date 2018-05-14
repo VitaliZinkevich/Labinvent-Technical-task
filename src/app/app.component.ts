5807f7727eaad84edeb36be0d82790db37839785
Vitalizinkevich <vitalizinkevich@gmail.com>
2018-05-14 17:55:12 +0300

    mostly some css adding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormArray} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements  AfterViewInit {
+networkForm: FormGroup
+
+
+
+constructor (private  fb: FormBuilder){
+
+  this.networkForm = fb.group ({
+    ethernetSettings: fb.group({
+         obtainIPAuto: 'true',
+         manualIpSettings:fb.group({
+            ip:['', Validators.required],
+            mask:['', Validators.required],
+            gateway:''
+          }),
+        dnsAuto: 'true',
+        manualDnsSettings: fb.group({
+          preferedDns:['', Validators.required],
+          alternativeDns:''
+
+        })
+       })/*,
+       wirelessSettings: fb.group({
+
+        */
+       })
+
+       this.onChanges();
+       this.ethernetFormDisabler();
+       this.dnsFormDisabler();
+
+
+
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  onChanges(): void {
+  this.networkForm.get('ethernetSettings').get('obtainIPAuto').valueChanges.subscribe(val => {
+    this.ethernetFormDisabler ();
+  });
+
+  this.networkForm.get('ethernetSettings').get('dnsAuto').valueChanges.subscribe(val => {
+    this.dnsFormDisabler ();
+  });
+}
+
+
+ethernetFormDisabler (){
+
+  if(this.networkForm.get('ethernetSettings').get('obtainIPAuto').value === 'false') {
+          this.networkForm.get('ethernetSettings').get('manualIpSettings').enable()
+
+  } else {
+          this.networkForm.get('ethernetSettings').get('manualIpSettings').disable()
+  }
+}
+
+dnsFormDisabler (){
+
+  if(this.networkForm.get('ethernetSettings').get('dnsAuto').value === 'false') {
+        this.networkForm.get('ethernetSettings').get('manualDnsSettings').enable()
+
+  } else {
+        this.networkForm.get('ethernetSettings').get('manualDnsSettings').disable()
+  }
+}
+
+
 }
